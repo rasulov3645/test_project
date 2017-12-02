cf908bbd449869ad656876575a59c4a956e717ad
Rasulov Gamzat <c0mrade3645@gmail.com>
2017-12-03 00:45:28 +0300

    edit models

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 # Create your models here.
+from products.models import Product
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=24, blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "Статус %s" % self.id
+
+    class Meta:
+        verbose_name = 'Статус заказа'
+        verbose_name_plural = 'Статусы заказа'
 
 
 
@@ -9,11 +24,12 @@ class Order(models.Model):
     customer_email = models.EmailField(blank=True, null=True, default=None)
     customer_phone = models.CharField(max_length=48, blank=True, null=True, default=None)
     comments = models.TextField(blank=True, null=True, default=None)
+    status = models.ForeignKey(Status)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return "Заказ %s" % self.id
+        return "Заказ %s %s" % (self.id, self.status.name)
 
     class Meta:
         verbose_name = 'Заказ'
@@ -23,6 +39,7 @@ class Order(models.Model):
 class ProductInOrder(models.Model):
     order = models.ForeignKey(Order, blank=True, null=True, default=None)
     product = models.ForeignKey(Product, blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
@@ -32,22 +49,3 @@ class ProductInOrder(models.Model):
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
